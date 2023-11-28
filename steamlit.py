b0cff20db3969d1e88946554a2fa604cd629652f
Sune Fibæk <56259443+sunefibaek@users.noreply.github.com>
2023-11-28 13:42:06 +0100

    Added download excel functionality

--- steamlit.py
+++ steamlit.py
@@ -17,7 +17,16 @@ df = create_dataframe()
 # Add a button to refresh the page  
 if st.button('Refresh Data'):  
     df = create_dataframe()  
-  
+
+# Add a button to convert the DataFrame to Excel  
+if st.button('Convert to Excel'):  
+    towrite = io.BytesIO()  
+    downloaded_file = df.to_excel(towrite, encoding='utf-8', index=False, header=True)  
+    towrite.seek(0)  # reset pointer  
+    b64 = base64.b64encode(towrite.read()).decode()  # some strings  
+    linko= f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="myfilename.xlsx">Download Excel File</a>'  
+    st.markdown(linko, unsafe_allow_html=True)
+
 # Add a multiselect dropdown to select which columns are visible  
 columns = st.multiselect("Select the columns to display", df.columns.tolist(), default=df.columns.tolist())  
 st.dataframe(df[columns])  
