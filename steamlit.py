036088292b8b8fe57bfc61e98c4799636b60222d
Sune Fibæk <56259443+sunefibaek@users.noreply.github.com>
2023-11-28 13:18:41 +0100

    Update steamlit.py

--- steamlit.py
+++ steamlit.py
@@ -1,24 +1,23 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-# Create a dictionary with four keys (col1, col2, col3, col4)
-# The value for each is a list of 8 random numbers
-
-def create_dataframe():
-    # Create a dictionary with four keys (col1, col2, col3, col4)
-    # The value for each is a list of 8 random numbers
-    data = {
-        'col1': np.random.rand(8),
-        'col2': np.random.rand(8),
-        'col3': np.random.rand(8),
-        'col4': np.random.rand(8),
-    }
-
-    # Create the DataFrame using the dictionary
-    df = pd.DataFrame(data)
-    return df
-
-df = create_dataframe()
-st.write('DataFrame:')
-st.dataframe(df)
+import streamlit as st  
+import pandas as pd  
+import numpy as np  
+  
+def create_dataframe():  
+    data = {  
+        'col1': np.random.rand(8),  
+        'col2': np.random.rand(8),  
+        'col3': np.random.rand(8),  
+        'col4': np.random.rand(8),  
+    }  
+    df = pd.DataFrame(data)  
+    return df  
+  
+df = create_dataframe()  
+  
+# Add a button to refresh the page  
+if st.button('Refresh Data'):  
+    df = create_dataframe()  
+  
+# Add a multiselect dropdown to select which columns are visible  
+columns = st.multiselect("Select the columns to display", df.columns.tolist(), default=df.columns.tolist())  
+st.dataframe(df[columns])  
