ecb8bf20c43a6a3e69c004186e24de365700db0a
Sune Fibæk <56259443+sunefibaek@users.noreply.github.com>
2023-11-28 13:09:54 +0100

    Update steamlit.py

--- steamlit.py
+++ steamlit.py
@@ -1,3 +1,25 @@
-import streamlit as st  
-# print("Hello, World!")  
-st.write("Hello, World!")  
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Create a dictionary with four keys (col1, col2, col3, col4)
+# The value for each is a list of 8 random numbers
+
+def create_dataframe():
+    # Create a dictionary with four keys (col1, col2, col3, col4)
+    # The value for each is a list of 8 random numbers
+    data = {
+        'col1': np.random.rand(8),
+        'col2': np.random.rand(8),
+        'col3': np.random.rand(8),
+        'col4': np.random.rand(8),
+    }
+
+    # Create the DataFrame using the dictionary
+    df = pd.DataFrame(data)
+    return df
+
+# Print the DataFrame
+
+# print("Hello, World!")
+st.dataframe(df)
